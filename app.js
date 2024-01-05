bb3e729717d24b9ed99902dbe4e94183e7500096
Advik <github@advik.dev>
2024-01-05 22:50:48 +0530

    Added express,js test server

---
+++ app.js
@@ -0,0 +1,27 @@
+// let http = require('http');
+
+
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end("First node js server!");
+// }).listen(7890);
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send("I'm here!");
+})
+
+app.get('/:url', function (req, res) {
+    let shortUrl = req.params.url;
+    res.send("The short url is: " + shortUrl);
+})
+
+app.listen(7890, function (req, res) {
+    console.log("Server is running....");
+});
+
+// console.log(module);
+// console.log(process);
