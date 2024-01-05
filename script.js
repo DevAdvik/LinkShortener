c9223427112f9a2e32d11678014df9cb8d58ee43
Advik <github@advik.dev>
2024-01-05 18:36:48 +0530

    Renamed file

--- script.js
+++
@@ -1,7 +0,0 @@
-const url = 'https://api.jsonsilo.com/a2876857-4573-4211-9576-b848fd593e59';
-
-const headers = {
-    'X-SILO-KEY': 'JGeOCTc4pVMRpM2sUdl5SnNWRCJhWDJJNb8DZSHNpY',
-    'Content-Type': 'application/json'
-};
-
