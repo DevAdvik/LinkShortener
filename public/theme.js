1f347a4e8c3aa2d80ff4b1a267d6c99c1205fa4d
Advik <github@advik.dev>
2024-01-20 03:41:57 +0530

    UI Changes and theme switcher

---
+++ public/theme.js
@@ -0,0 +1,14 @@
+const root = document.querySelector(":root");
+
+let isDark = true;
+document.querySelector(".themeSelector").addEventListener("click", (ev) => {
+    isDark = (ev.target.id === 'darkTheme') ? true : (ev.target.id === 'lightTheme') ? false : isDark; //Basically if else-if else
+
+    root.style.setProperty("--main-bg", isDark ? "#0d0f15" : "#e6e6e6");
+    root.style.setProperty("--sec-bg", isDark ? "#1f232a" : "#ffffff");
+    root.style.setProperty("--text-color", isDark ? "#ffffff" : "#1C1B1E");
+    root.style.setProperty("--inverse-text-color", isDark ? "#1C1B1E" : "#ffffff");
+    root.style.setProperty("--footer-color", isDark ? "#e6e6e6" : "#1f232a");
+    root.style.setProperty("--flashy-blue", isDark ? "#2764d6" : "#3b7df8");
+
+})
